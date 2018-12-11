426485ff8f16cb25a134a343ef13029acb02e671
Vivian <chrysophyta@gmail.com>
2018-12-11 23:51:08 +0800

    change route to /auth/google

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -15,10 +15,10 @@ class App extends Component {
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Learn React
           </a>
+          <a href="/auth/google">Sign in with Google</a>
         </header>
       </div>
     );
