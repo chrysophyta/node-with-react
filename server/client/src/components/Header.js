4ab2cd9dcebaed694a9019f8e48071b691c1fe5a
Vivian <chrysophyta@gmail.com>
2019-01-06 23:55:40 +0800

    refactor fetchUser

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,7 +5,9 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <a href="#" className="left brand-logo">
+            Emaily
+          </a>
           <ul className="right">
             <li>
               <a>Login With Google</a>
