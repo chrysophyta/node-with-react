66bcd70eb49e04948c89a7b2ad34bb801c97221a
Vivian <chrysophyta@gmail.com>
2019-01-07 22:43:19 +0800

    fix bug

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -5,3 +5,11 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
   dispatch({ type: FETCH_USER, payload: res });
 };
+
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios
+//       .get('/api/current_user')
+//       .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+// };
