481212b182831b75f5e11950eb50e6a1123a4581
Vivian <chrysophyta@gmail.com>
2019-01-13 23:44:27 +0800

    add payment button with react-stripe-checkout

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -18,3 +18,6 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#root')
 );
+
+console.log('stripekey is', process.env.REACT_APP_STRIPE_KEY);
+console.log('environment is', process.env.NODE_ENV);
