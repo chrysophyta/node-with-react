a70f6a2f93ce531f838f467ca5f03ab95c9941c3
Vivian <chrysophyta@gmail.com>
2019-01-23 23:37:53 +0800

    fix typos

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,8 +1,8 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
-const requreLogin = require("../middlewares/requireLogin");
+const requireLogin = require('../middlewares/requireLogin');
+module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
-
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
